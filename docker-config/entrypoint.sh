388912dae2882d49ab3725d3ef0ea110a3e7e192
Nehemias Mercau <nehemias.mercau.-nd@disney.com>
2025-09-15 22:58:40 -0300

    ...

--- docker-config/entrypoint.sh
+++ docker-config/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 : "${VITE_API_BASE_URL:=http://localhost:8080/}"
 
 # Copiar el template a config.js y reemplazar el placeholder
-cp /app/dist/config.template.js /app/dist/config.js
+cp /app/dist/config-template.js /app/dist/config.js
 sed -i "s|__API_BASE_URL__|${VITE_API_BASE_URL}|g" /app/dist/config.js
 
 exec "$@"
